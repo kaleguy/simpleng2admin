964c14ce507463702cbbd93dd7f0d29df410d8fc
Angular CLI <angular-cli@angular.io>
2017-02-04 05:52:59 -0500

    chore: initial commit from @angular/cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SimpleAdminPage } from './app.po';
+
+describe('simple-admin App', function() {
+  let page: SimpleAdminPage;
+
+  beforeEach(() => {
+    page = new SimpleAdminPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
