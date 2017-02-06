49fde95a1b1ce939807550c63892724cbaf4b682
Joe Orr <joe.orr@gmail.com>
2017-02-06 04:43:09 -0500

    errors

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -8,62 +8,74 @@ import * as moment from 'moment/moment';
 
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.sass']
+    selector: 'app-weather',
+    templateUrl: './weather.component.html',
+    styleUrls: ['./weather.component.sass']
 })
 export class WeatherComponent implements OnInit {
 
-  private baseWeatherURL: string= 'http://api.openweathermap.org/data/2.5/weather?q=';
-  private urlSuffix: string = ",US&units=imperial&appid=c478bded4945b00879ff8199ec9aed58";
+    private baseWeatherURL:string = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    private urlSuffix:string = "&units=imperial&appid=c478bded4945b00879ff8199ec9aed58";
 
-  reportHidden: boolean = true;
-  pending: boolean = false;
+    reportHidden:boolean = true;
+    pending:boolean = false;
 
-  report: any = {
-    main: '',
-    weather: [{main:''}],
-    coord: {dt:''},
-    sys: ''
-  };
-
-  constructor(private http:Http){
-  }
-
-  getWeather(city: string) {
-
-    this.pending = true;
-    this.reportHidden = true;
-
-    this.http.get(this.baseWeatherURL + city + this.urlSuffix)
-        .map(res => {
-          console.log(res);
-          return res.json()}
-        )
-        .delay(1500)
-        .subscribe(
-            res => {
-
-              this.pending = false;
-
-              if (res['cod'] === '404') return;
-
-              if (!res.main) {
-                this.report.message ='City is not found.';
-              } else {
-                this.reportHidden = false;
-                this.report = res;
-                console.log("Weather Report: ", this.report);
-              }
-            },
-            err => console.log(`Can't get weather. Error code: %s, URL: %s`, err.message, err.url),
-            () => console.log(`Weather is retrieved`)
-        );
-
-  }
+    report:any = {
+        main: '',
+        weather: [{main: ''}],
+        coord: {dt: ''},
+        sys: '',
+        message: {
+            cod: '',
+            message: ''
+        }
+    };
 
 
-  ngOnInit(){}
+
+    constructor(private http:Http) {
+    }
+
+    getWeather(city:string) {
+
+        this.pending = true;
+        this.report.message = '';
+        this.reportHidden = true;
+
+        this.http.get(this.baseWeatherURL + city + this.urlSuffix)
+            .map(res => {
+                    console.log(res);
+                    return res.json()
+                }
+            )
+            .delay(1500)
+            .subscribe(
+                res => {
+
+                    this.pending = false;
+
+                    if (res.cod === '404') return;
+                    if (res.cod >= '500') {
+                        this.report.message = res.message;
+                        return;
+                    }
+
+                    this.reportHidden = false;
+                    this.report = res;
+                },
+                err => {
+                    this.report.message = JSON.parse(err._body);
+                    this.report.message.status = err.status;
+                    this.pending = false;
+                },
+                () => console.log(`Weather is retrieved`)
+            )
+
+    }
+
+
+    ngOnInit() {
+    }
 
 
 }
