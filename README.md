9bf3f231b1a718dae5abe31ef1f4787bf86e3725
Joe Orr <joe.orr@gmail.com>
2017-02-08 05:50:58 -0500

    500 test

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class/module`.
 
+## Code scaffolding
+
+Run `ng lint` to have tslint check your code.
+
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
--- src/app/weather/weather.component.spec.ts
+++ src/app/weather/weather.component.spec.ts
@@ -12,201 +12,192 @@ import {MockBackend, MockConnection} from '@angular/http/testing';
 
 import { WeatherComponent } from './weather.component';
 
+// Next test batch directly calls the API without a mock. It can be
+// disabled once E2E tests are in place.
 describe('WeatherComponent', () => {
 
-  let component: WeatherComponent;
-  let fixture: ComponentFixture<WeatherComponent>;
-  let injector: Injector;
-  let element;
+    let component: WeatherComponent;
+    let fixture: ComponentFixture<WeatherComponent>;
+    let injector: Injector;
+    let element;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpModule
-      ],
-      declarations: [ WeatherComponent ],
-      providers: [
-      ]
-    })
-    .compileComponents();
-    injector = getTestBed();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+                imports: [
+                    HttpModule
+                ],
+                declarations: [ WeatherComponent ],
+                providers: [
+                ]
+            })
+            .compileComponents();
+        injector = getTestBed();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
-  });
-
-  xit('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should retrieve the results for New York ', (done) => {
-
-    component = fixture.componentInstance;
-    component.getWeatherService('New York', 0)
-        .subscribe(
-            res => {
-              expect(res.name).toEqual('New York');
-              done();
-            },
-            err => {
-              console.log(err);
-              expect(true).toEqual(false);
-              done();
-            },
-            () => console.log(`Weather is retrieved`)
-        );
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WeatherComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
 
-  xit('should display the results for New York ', (done) => {
-
-    fixture = TestBed.createComponent(WeatherComponent);
-    element = fixture.nativeElement;
-    component = fixture.componentInstance;
-    component.getWeather('New York', 0, () => {
-      fixture.detectChanges();
-      expect(element.querySelector('#name').innerHTML).toBe('New York');
-      done();
+    beforeEach(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
     });
 
-  });
-
-  xit('should display an error for city = koajfwe ', (done) => {
-
-    fixture = TestBed.createComponent(WeatherComponent);
-    element = fixture.nativeElement;
-    component = fixture.componentInstance;
-    component.getWeather('koajfwe', 0, () => {
-      fixture.detectChanges();
-      expect(element.querySelector('#message').innerHTML).toContain('Not found');
-      done();
+    it('should create', () => {
+        expect(component).toBeTruthy();
     });
 
-  });
+    it('should retrieve the results for New York ', (done) => {
+
+        component = fixture.componentInstance;
+        component.getWeatherService('New York', 0)
+            .subscribe(
+                res => {
+                    expect(res.name).toEqual('New York');
+                    done();
+                },
+                err => {
+                    console.log(err);
+                    expect(true).toEqual(false);
+                    done();
+                },
+                () => console.log(`Weather is retrieved`)
+            );
+    });
+
+    it('should display the results for New York ', (done) => {
+
+        fixture = TestBed.createComponent(WeatherComponent);
+        element = fixture.nativeElement;
+        component = fixture.componentInstance;
+        component.getWeather('New York', 0, () => {
+            fixture.detectChanges();
+            expect(element.querySelector('#name').innerHTML).toBe('New York');
+            done();
+        });
+
+    });
+
+    it('should display an error for city = koajfwe ', (done) => {
+
+        fixture = TestBed.createComponent(WeatherComponent);
+        element = fixture.nativeElement;
+        component = fixture.componentInstance;
+        component.getWeather('koajfwe', 0, () => {
+            fixture.detectChanges();
+            const s = element.querySelector('#message').innerHTML;
+            s = s.toLowerCase();
+            expect(s).toContain('not found');
+            done();
+        });
+
+    });
 
 });
 
 
 describe('WeatherComponent with Mocks', () => {
-  let component: WeatherComponent;
-  let fixture: ComponentFixture<WeatherComponent>;
-  // let mockBackend: MockBackend;
-  let injector: Injector;
+    let component: WeatherComponent;
+    let fixture: ComponentFixture<WeatherComponent>;
+    let element;
+    let injector: Injector;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-          imports: [
-            HttpModule
-          ],
-          declarations: [ WeatherComponent ],
-          providers: [
-            MockBackend,
-            BaseRequestOptions,
-            {
-              provide: Http,
-              useFactory: (backend, options) => new Http(backend, options),
-              deps: [MockBackend, BaseRequestOptions]
-            }
-          ]
-        })
-        .compileComponents();
-    injector = getTestBed();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+                imports: [
+                    HttpModule
+                ],
+                declarations: [ WeatherComponent ],
+                providers: [
+                    MockBackend,
+                    BaseRequestOptions,
+                    {
+                        provide: Http,
+                        useFactory: (backend, options) => new Http(backend, options),
+                        deps: [MockBackend, BaseRequestOptions]
+                    }
+                ]
+            })
+            .compileComponents();
+        injector = getTestBed();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WeatherComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  beforeEach(() => {
-    // mockBackend = injector.get(XHRBackend);
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
-  });
+    beforeEach(() => {
+        // mockBackend = injector.get(XHRBackend);
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+    });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
 
-  describe('Weather Service Mocks', () => {
-    const mockResponse = {
-      city: 'foo'
-    };
+    describe('Weather Service Mocks', () => {
+        const mockResponse = {
+            city: 'foo'
+        };
 
-    it('should parse response', async(inject(
-        [MockBackend], (mockBackend) => {
+        it('should parse response', async(inject(
+            [MockBackend], (mockBackend) => {
 
-          mockBackend.connections.subscribe(conn => {
-            conn.mockRespond(
-                new Response(new ResponseOptions({ body: JSON.stringify(mockResponse) }))
-            );
-          });
+                mockBackend.connections.subscribe(conn => {
+                    conn.mockRespond(
+                        new Response(new ResponseOptions({ body: JSON.stringify(mockResponse) }))
+                    );
+                });
 
-          fixture = TestBed.createComponent(WeatherComponent);
-          // element = fixture.nativeElement;
-          component = fixture.componentInstance;
-          component.getWeatherService('New York', 0)
-              .subscribe(
-                  res => {
-                    expect(res).toEqual({
-                      city: 'foo'
-                    });
-                  },
-                  err => {
-                    console.log(err);
-                  },
-                  x => console.log('WS test complete')
+                fixture = TestBed.createComponent(WeatherComponent);
+                // element = fixture.nativeElement;
+                component = fixture.componentInstance;
+                component.getWeatherService('New York', 0)
+                    .subscribe(
+                        res => {
+                            expect(res).toEqual({
+                                city: 'foo'
+                            });
+                        },
+                        err => {
+                            console.log(err);
+                        },
+                        x => console.log('WS test complete')
 
-              );
+                    );
 
-          // component.get
+                // component.get
 
-        }))
-    );
-    it('should display error message for 500', async(inject(
-        [MockBackend], (mockBackend) => {
+            }))
+        );
+        it('should display error message for 500', async(inject(
+            [MockBackend], (mockBackend) => {
 
-          mockBackend.connections.subscribe(conn => {
-            conn.mockRespond(
-                new Response(new ResponseOptions({ body: JSON.stringify(mockResponse) }))
-            );
-          });
+                mockBackend.connections.subscribe(conn => {
+                    conn.mockError(new Response(new ResponseOptions({
+                        body: JSON.stringify({ error: 'Internal Server Error' }),
+                        status: 500,
+                    })));
+                });
 
-          fixture = TestBed.createComponent(WeatherComponent);
-          // element = fixture.nativeElement;
-          component = fixture.componentInstance;
-          component.getWeatherService('New York', 0)
-              .subscribe(
-                  res => {
-                    expect(res).toEqual({
-                      city: 'foo'
-                    });
-                  },
-                  err => {
-                    console.log(err);
-                  },
-                  x => console.log('WS test complete')
 
-              );
+                fixture = TestBed.createComponent(WeatherComponent);
+                element = fixture.nativeElement;
+                component = fixture.componentInstance;
+                component.getWeather('koajfwe', 0, () => {
+                    fixture.detectChanges();
+                    expect(element.querySelector('#err').innerHTML).toContain('500');
+                  //  done();
+                });
 
-/*
-          conn.mockError(new Response(new ResponseOptions({
-            body: JSON.stringify({ error: 'Internal Server Error' }),
-            status: 500,
-          })));
-*/
+            }))
+        );
 
-        }))
-    );
-
-  }); // describe
+    }); // describe
 
 
 });
